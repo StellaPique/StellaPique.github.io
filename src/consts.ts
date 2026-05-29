83d44a146ddcbb39f9b644ce83a0d3630698872c
StellaPique <gabriel.novak2008@gmail.com>
2026-05-29 11:08:33 +0200

    Checkpoint before everything breaks

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Via Stellarum';
+export const SITE_DESCRIPTION = 'What are you looking at, dickweed?';
+export const PrimColour = "#676afa"
